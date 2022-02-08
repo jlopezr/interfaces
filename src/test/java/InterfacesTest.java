9187ada062ed63f8cb18ef2510fb908ac0d962e4
Juan López Rubio <j.lopez.r@gmail.com>
2022-02-08 20:54:27 +0100

    show marks in console

--- src/test/java/InterfacesTest.java
+++ src/test/java/InterfacesTest.java
@@ -27,7 +27,7 @@ public class InterfacesTest {
     }
 
     @Test
-    public void test_circle_methods() {
+    public void test_0_circle_methods() {
         Circle c = new Circle(5.0);
         assertThat(c.getArea()).isEqualTo(Math.PI*Math.pow(5.0,2.0));
         assertThat(c.getPerimeter()).isEqualTo(2*Math.PI*5);
@@ -36,7 +36,7 @@ public class InterfacesTest {
     }
 
     @Test
-    public void test_square_methods() {
+    public void test_1_square_methods() {
         Square c = new Square(8);
         assertThat(c.getArea()).isEqualTo(8*8);
         assertThat(c.getPerimeter()).isEqualTo(8*4);
@@ -45,7 +45,7 @@ public class InterfacesTest {
     }
 
     @Test
-    public void test_rectangle_methods() {
+    public void test_2_rectangle_methods() {
         Rectangle c = new Rectangle(2,5);
         assertThat(c.getArea()).isEqualTo(2*5);
         assertThat(c.getPerimeter()).isEqualTo(2*2+2*5);
@@ -54,7 +54,7 @@ public class InterfacesTest {
     }
 
     @Test
-    public void test_totalArea() {
+    public void test_3_totalArea() {
         Circle a = new Circle(5.0);
         Square b = new Square(8);
         Rectangle c = new Rectangle(2,5);
@@ -66,11 +66,11 @@ public class InterfacesTest {
 
         assertThat(f.calculateTotalArea()).isEqualTo(152.53981633974485);
 
-        addComment(1, "");
+        addComment(0.5, "");
     }
 
     @Test
-    public void test_totalPerimeter() {
+    public void test_4_totalPerimeter() {
         Circle a = new Circle(5.0);
         Square b = new Square(8);
         Rectangle c = new Rectangle(2,5);
@@ -81,6 +81,8 @@ public class InterfacesTest {
         f.addNewFigure(c);
 
         assertThat(f.calculateTotalPerimeter()).isEqualTo(77.41592653589794);
+
+        addComment(0.5, "");
     }
 
     private Class getInterfaceOrSuperClass(Class c) {
@@ -93,7 +95,7 @@ public class InterfacesTest {
     }
 
     @Test
-    public void test_inheritance() throws Exception {
+    public void test_5_inheritance() throws Exception {
         Class circleParent = getInterfaceOrSuperClass(Class.forName("Circle"));
         Class rectangleParent = getInterfaceOrSuperClass(Class.forName("Rectangle"));
         Class squareParent = getInterfaceOrSuperClass(Class.forName("Square"));
@@ -102,10 +104,12 @@ public class InterfacesTest {
         assertThat(circleParent).as("All figures should have a common ancestor").isEqualTo(squareParent);
         assertThat(circleParent).as("FigureManager should allow only to add figures").isNotEqualTo(Class.forName("java.lang.Object"));
         assertThat(circleParent.isInterface()).as("Empty classes should be interfaces").isEqualTo(true);
+
+        addComment(3, "");
     }
 
     @Test
-    public void test_manager_accepts_other_figures() throws Exception {
+    public void test_6_manager_accepts_other_figures() throws Exception {
         class TestFigure extends Circle {
 
             public TestFigure() {
@@ -125,18 +129,25 @@ public class InterfacesTest {
         manager.addNewFigure(new TestFigure());
         assertThat(manager.calculateTotalArea()).as("FigureManager must accept new implemented figures").isEqualTo(-1);
         assertThat(manager.calculateTotalPerimeter()).as("FigureManager must accept new implemented figures").isEqualTo(-2);
+
+        addComment(3, "");
     }
 
-    //@Test
+    @Test
     public void x_show_mark() {
         StringBuffer buffer = new StringBuffer();
+
 
         for(String comment : comments) {
             buffer.append(comment);
             buffer.append("\n");
         }
 
-        fail("Summary:\n"+buffer+"\nTotal: "+mark+"\n");
+
+        //fail("Summary:\n"+buffer+"\nTotal: "+mark+"\n");
+        System.out.println("\n**************************************************");
+        System.out.println("Summary:\n"+buffer+"\nTotal: "+mark);
+        System.out.println("**************************************************\n");
     }
 
 }
